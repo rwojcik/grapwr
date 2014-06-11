5d9a864bc858f6b5c6e4c15aed9a24dc706093a4
Rafal Wojcik <rafalwojcik3@gmail.com>
2014-06-12 00:26:22 +0200

    Poprawa haxa oraz próba naprawy pliku pytań

--- GraPWr/src/pl/grapwr/activity/GameActivity.java
+++ GraPWr/src/pl/grapwr/activity/GameActivity.java
@@ -12,10 +12,11 @@ import android.app.Activity;
 import android.app.Fragment;
 import android.content.Intent;
 import android.graphics.Color;
-import android.graphics.PorterDuff.Mode;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
@@ -182,27 +183,27 @@ public class GameActivity extends Activity {
 		answered = true;
 	}
 
-//	@Override
-//	public boolean onCreateOptionsMenu(Menu menu) {
-//
-//		// Inflate the menu; this adds items to the action bar if it is present.
-//		getMenuInflater().inflate(R.menu.game, menu);
-//		return true;
-//	}
-//
-//	@Override
-//	public boolean onOptionsItemSelected(MenuItem item) {
-//		// Handle action bar item clicks here. The action bar will
-//		// automatically handle clicks on the Home/Up button, so long
-//		// as you specify a parent activity in AndroidManifest.xml.
-//		int id = item.getItemId();
-//		if (id == R.id.action_settings) {
-//			Intent i = new Intent(this, FinishActivity.class);
-//			this.startActivity(i);
-//			return true;
-//		}
-//		return super.onOptionsItemSelected(item);
-//	}
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.game, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			Intent i = new Intent(this, FinishActivity.class);
+			this.startActivity(i);
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 
 	/**
 	 * A placeholder fragment containing a simple view.
